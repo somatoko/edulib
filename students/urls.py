b14ba1740e1ba7c52f5c8d93210c990714b58dfa
somatoko <3844353+somatoko@users.noreply.github.com>
2026-04-24 17:11:53 +0200

    feat: add caching support

--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 
@@ -6,6 +7,13 @@ urlpatterns = [
     path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
     path('enroll-course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
     path('courses/', views.StudentCourseListView.as_view(), name='student_course_list'),
-    path('courses/<pk>/', views.StudentCourseDetailView.as_view(), name='student_course_detail'),
-    path('courses/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+    path(
+        'courses/<pk>/',
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+        name='student_course_detail'
+    ),
+    path('courses/<pk>/<module_id>/',
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         name='student_course_detail_module'
+    ),
 ]
