90b2d99ca801768884013055ac9af766fdb1a239
somatoko <3844353+somatoko@users.noreply.github.com>
2026-04-24 20:27:09 +0200

    feat: added API endpoints to subjects and courses

---
+++ courses/api/views.py
@@ -0,0 +1,32 @@
+from django.db.models import Count
+from rest_framework import generics
+from rest_framework import viewsets
+from courses.api.pagination import StandardPagination
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
+
+
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(
+#         total_courses=Count('courses')
+#     )
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(
+#         total_courses=Count('courses')
+#     )
+#     serializer_class = SubjectSerializer
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
