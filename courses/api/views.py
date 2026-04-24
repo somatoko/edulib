0a5dad142d6c586d7437a5b0c197475ecb30e88c
somatoko <3844353+somatoko@users.noreply.github.com>
2026-04-24 20:45:14 +0200

    feat: added capability for logged-in users to enroll in a course

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,24 +1,16 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
 
-
-# class SubjectListView(generics.ListAPIView):
-#     queryset = Subject.objects.annotate(
-#         total_courses=Count('courses')
-#     )
-#     serializer_class = SubjectSerializer
-#     pagination_class = StandardPagination
-
-
-# class SubjectDetailView(generics.RetrieveAPIView):
-#     queryset = Subject.objects.annotate(
-#         total_courses=Count('courses')
-#     )
-#     serializer_class = SubjectSerializer
 
 class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
@@ -30,3 +22,28 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+
+# class CourseEnrollView(APIView):
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
